083c729e6df728d1628466c859f1815b54d36307
Olga Melman <olga.melman198237@gmail.com>
2023-05-19 14:11:10 +0300

    added positive1 test

---
+++ src/test/java/TestFormCardOrder.java
@@ -0,0 +1,42 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.*;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class TestFormCardOrder {
+    private WebDriver driver;
+    @BeforeAll
+    static void setUpAll() {
+        WebDriverManager.chromedriver().setup();
+    }
+    @BeforeEach
+    public  void setUp() {
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--headless");
+        driver = new ChromeDriver(options);
+    }
+    @AfterEach
+    void tearDown() {
+        driver.quit();
+        driver = null;
+    }
+
+    @Test
+    void shouldTestV1() {
+        driver.get("http://localhost:9999");
+        driver.findElement(By.cssSelector("[data-test-id='name'] input")).sendKeys("Эмилия-Анна ");
+        driver.findElement(By.cssSelector("[data-test-id='phone'] input")).sendKeys("+79994567373");
+        driver.findElement(By.cssSelector("[data-test-id='agreement']")).click();
+        driver.findElement(By.tagName("button")).click();
+        String expected = "Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.";
+        String actual = driver.findElement(By.tagName("p")).getText().trim();
+        assertEquals(expected, actual);
+
+    }
+}
